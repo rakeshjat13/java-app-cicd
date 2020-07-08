f9918dabf7a5172af92db4602637f5fc1fb85bc5
rakesh13 <rakesh@amwebtech.com>
2020-07-08 12:07:19 +0530

    add java files

---
+++ src/test/java/com/testexample/aws/AwsElasticBeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testexample.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
